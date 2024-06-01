ed14c99a9b8ccc7c503dddccdb043692cb3d82fa
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-01 22:32:46 +0700

    move initializeDatabase to PacketLogger

--- src/main/java/net/meowcorp/mod/rewind/util/DatabaseHelper.java
+++ src/main/java/net/meowcorp/mod/rewind/util/DatabaseHelper.java
@@ -6,23 +6,5 @@ import java.io.File;
 import java.sql.*;
 
 public class DatabaseHelper {
-	public static void initializeDatabase() {
-		try (Connection conn = DriverManager.getConnection(Rewind.DATABASE_PATH)) {
-			if (conn != null) {
-				DatabaseMetaData meta = conn.getMetaData();
-				try (Statement statement = conn.createStatement()) {
-					String sql =
-							"CREATE TABLE IF NOT EXISTS packets (" +
-									"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-									"timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'NOW')), " +
-									"packetType TEXT NOT NULL, " +
-									"packetData BLOB)";
-					statement.execute(sql);
-					Rewind.LOGGER.info("Database initialized successfully: {}", new File(Rewind.DATABASE_PATH).getAbsolutePath());
-				}
-			}
-		} catch (SQLException e) {
-			Rewind.LOGGER.error("Failed to connect to database: {}", e.getMessage());
-		}
-	}
+
 }
--- src/main/java/net/meowcorp/mod/rewind/util/PacketLogger.java
+++ src/main/java/net/meowcorp/mod/rewind/util/PacketLogger.java
@@ -1,7 +1,10 @@
 package net.meowcorp.mod.rewind.util;
 
+import net.meowcorp.mod.rewind.Rewind;
 import net.minecraft.network.packet.Packet;
 
+import java.io.File;
+import java.sql.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,6 +15,26 @@ public class PacketLogger {
 	private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
 	public PacketLogger() {
+		initializeDatabase();
+	}
 
+	private void initializeDatabase() {
+		try (Connection conn = DriverManager.getConnection(Rewind.DATABASE_PATH)) {
+			if (conn != null) {
+				DatabaseMetaData meta = conn.getMetaData();
+				try (Statement statement = conn.createStatement()) {
+					String sql =
+							"CREATE TABLE IF NOT EXISTS packets (" +
+									"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+									"timestamp DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'NOW')), " +
+									"packetType TEXT NOT NULL, " +
+									"packetData BLOB)";
+					statement.execute(sql);
+					Rewind.LOGGER.info("Database initialized successfully: {}", new File(Rewind.DATABASE_PATH).getAbsolutePath());
+				}
+			}
+		} catch (SQLException e) {
+			Rewind.LOGGER.error("Failed to connect to database: {}", e.getMessage());
+		}
 	}
 }
