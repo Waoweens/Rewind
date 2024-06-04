6484932e96b3393a7f2eb262741e20a12acb1f00
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-04 13:10:50 +0700

    get database path from world folder

--- src/main/java/net/meowcorp/mod/rewind/Rewind.java
+++ src/main/java/net/meowcorp/mod/rewind/Rewind.java
@@ -6,19 +6,22 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.meowcorp.mod.rewind.command.CommandRegistry;
 import net.meowcorp.mod.rewind.packet.PacketSerializer;
 import net.meowcorp.mod.rewind.packet.PacketDatabase;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.WorldSavePath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
 
 public class Rewind implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("rewind");
-	public static final String DATABASE_PATH = "jdbc:sqlite:rewind.db";
+	public static String DATABASE_PATH;
 	public static final String MOD_ID = "rewind";
 
 	private static PacketDatabase packetLogger;
-
 	public static PacketSerializer SERIALIZER = new PacketSerializer();
 
 	@Override
@@ -28,14 +31,21 @@ public class Rewind implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-
-		packetLogger = new PacketDatabase();
 		LOGGER.info("Rewind packet logger initialized");
 
 		// Registry
 		CommandRegistry.register();
 
 		// events
+		ServerLifecycleEvents.SERVER_STARTED.register((MinecraftServer server) -> {
+			Path worldPath = server.getSavePath(WorldSavePath.ROOT);
+
+			String worldPathString = worldPath.resolve("rewind.db").toString();
+			DATABASE_PATH = "jdbc:sqlite:" + worldPathString;
+
+			packetLogger = new PacketDatabase();
+		});
+
 		ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
 			packetLogger.shutdown();
 			LOGGER.info("Rewind packet logger shutdown");
