9d1b66f9c57297bc5397b0535c24fdd754785f0f
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-04 13:10:39 +0700

    make dynamic

--- src/main/java/net/meowcorp/mod/rewind/packet/PacketStrategy.java
+++ src/main/java/net/meowcorp/mod/rewind/packet/PacketStrategy.java
@@ -4,39 +4,33 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import net.meowcorp.mod.rewind.util.IPacketStrategy;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.EntityS2CPacket;
 
-public abstract class PacketStrategy implements IPacketStrategy {
-	protected Gson gson = new Gson();
+public class PacketStrategy<T extends Packet<?>> implements IPacketStrategy<T> {
+	private final Class<T> packetClass;
+	private static final Gson gson = new Gson();
 
-	@Override
+	public PacketStrategy(Class<T> packetClass) {
+		this.packetClass = packetClass;
+	}
+
+	/**
+	 * Serialize a packet to a JSON object
+	 * @param packet generic {@link Packet} type
+	 * @return {@link JsonObject} containing packet data (fields)
+	 */
 	public JsonObject serialize(Packet<?> packet) {
 		// default
 		return gson.toJsonTree(packet).getAsJsonObject();
 	}
 
-	@Override
-	public abstract Packet<?> deserialize(JsonObject json);
-
-	// Subclasses
-
-	public static class EMoveRelative extends PacketStrategy {
-		@Override
-		public Packet<?> deserialize(JsonObject json) {
-			return null;
-		}
+	/**
+	 * Deserialize a JSON object to a packet
+	 * @param json {@link JsonObject} containing packet data
+	 * @return {@link Packet}-type class with fields from {@code json}
+	 */
+	public T deserialize(JsonObject json) {
+		return gson.fromJson(json, packetClass);
 	}
 
-	public static class ERotate extends PacketStrategy {
-		@Override
-		public Packet<?> deserialize(JsonObject json) {
-			return null;
-		}
-	}
-
-	public static 	class ERotateAndMoveRelative extends PacketStrategy {
-		@Override
-		public Packet<?> deserialize(JsonObject json) {
-			return null;
-		}
-	}
 }
